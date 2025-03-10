ea1357fb8cfdc8a7f9b01749d6c81f1af433a254
Jude Chen <judechen@microsoft.com>
2025-03-10 11:08:16 -0500

    initial version for existing vnet.

---
+++ Existing_VNET/main.tf
@@ -0,0 +1,96 @@
+resource "azapi_resource" "image-template" {
+  type      = "Microsoft.VirtualMachineImages/imageTemplates@2024-02-01"
+  name      = "Win2022Web-template"
+  parent_id = azurerm_resource_group.demo-rg.id
+  location  = var.location
+  body = {
+    identity = {
+      type = "UserAssigned"
+      userAssignedIdentities = {
+        (azurerm_user_assigned_identity.aibidentity.id) = {}
+      }
+    }
+    properties = {
+      buildTimeoutInMinutes = 0
+      customize = [
+        {
+          name = "create_folder"
+          type = "PowerShell"
+          inline = [
+            "New-Item -ItemType Directory C:\\ImageBuilderWebApp"
+          ]
+          runAsSystem = false
+          runElevated = true
+        },
+        {
+          name        = "webpagefile"
+          type        = "File"
+          sourceUri   = azurerm_storage_blob.webpage.url
+          destination = "C:\\ImageBuilderWebApp\\index.html"
+        },
+        {
+          name        = "setupwebsite"
+          type        = "PowerShell"
+          scriptUri   = azurerm_storage_blob.pwshscript.url
+          runAsSystem = false
+          runElevated = true
+        },
+        {
+          type        = "WindowsUpdate"
+          updateLimit = 0
+          filters = [
+            "exclude:$_.Title -like '*Preview*'",
+            "include:$true"
+          ]
+        },
+        {
+          type = "WindowsRestart"
+        }
+      ]
+      distribute = [
+        {
+          artifactTags      = {}
+          runOutputName     = "runOutputImageVersion"
+          type              = "SharedImage"
+          excludeFromLatest = false
+          galleryImageId    = azurerm_shared_image.demo-image.id
+          replicationRegions = [
+            var.location
+          ]
+        }
+      ]
+      optimize = {
+        vmBoot = {
+          state = "Enabled"
+        }
+      }
+      source = {
+        type      = "PlatformImage"
+        offer     = "WindowsServer"
+        publisher = "MicrosoftWindowsServer"
+        sku       = "2022-datacenter-azure-edition"
+        version   = "latest"
+      }
+      vmProfile = {
+        osDiskSizeGB = 127
+        vmSize       = "Standard_D2s_v3"
+        vnetConfig = {
+          subnetId = azurerm_virtual_network.demo-vnet.subnet[0].id
+        }
+      }
+    }
+  }
+  depends_on = [azurerm_role_assignment.gallery-role-assignment, azurerm_role_assignment.storage-role-assignment]
+}
+
+resource "terraform_data" "buildImage" {
+  triggers_replace = [timestamp()]
+
+  provisioner "local-exec" {
+    command = "az resource invoke-action --resource-group ${azurerm_resource_group.demo-rg.name} --resource-type Microsoft.VirtualMachineImages/imageTemplates -n ${azapi_resource.image-template.name} --action Run"
+  }
+
+  depends_on = [
+    azapi_resource.image-template
+  ]
+}
---
+++ Existing_VNET/prequisites.tf
@@ -0,0 +1,148 @@
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "demo-rg" {
+  location = var.location
+  name     = "${var.resource_prefix}-demo-rg"
+}
+
+resource "azurerm_virtual_network" "demo-vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.demo-rg.location
+  resource_group_name = azurerm_resource_group.demo-rg.name
+  subnet {
+    name                                          = "aib-subnet"
+    address_prefixes                              = ["10.0.0.0/26"]
+    private_link_service_network_policies_enabled = false
+  }
+  subnet {
+    name             = "vm-subnet"
+    address_prefixes = ["10.0.0.64/26"]
+  }
+}
+
+resource "azurerm_storage_account" "demo-sa" {
+  name                     = "${var.resource_prefix}sa${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.demo-rg.name
+  location                 = azurerm_resource_group.demo-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "demo-container" {
+  name                  = "${var.resource_prefix}-files"
+  storage_account_id    = azurerm_storage_account.demo-sa.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "webpage" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.demo-sa.name
+  storage_container_name = azurerm_storage_container.demo-container.name
+  type                   = "Block"
+  source                 = "../Artifacts/index.html"
+}
+
+resource "azurerm_storage_blob" "pwshscript" {
+  name                   = "setupwebsite.ps1"
+  storage_account_name   = azurerm_storage_account.demo-sa.name
+  storage_container_name = azurerm_storage_container.demo-container.name
+  type                   = "Block"
+  source                 = "../Artifacts/setupwebsite.ps1"
+}
+
+resource "azurerm_shared_image_gallery" "demo-sig" {
+  name                = "${var.resource_prefix}_${random_string.suffix.result}_gallery"
+  resource_group_name = azurerm_resource_group.demo-rg.name
+  location            = azurerm_resource_group.demo-rg.location
+  description         = "Shared Image Gallery for demo"
+}
+
+resource "azurerm_shared_image" "demo-image" {
+  name                     = "Win2022Web"
+  resource_group_name      = azurerm_resource_group.demo-rg.name
+  gallery_name             = azurerm_shared_image_gallery.demo-sig.name
+  location                 = azurerm_resource_group.demo-rg.location
+  os_type                  = "Windows"
+  hyper_v_generation       = "V2"
+  trusted_launch_supported = true
+  identifier {
+    publisher = "Contoso"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+  }
+}
+
+resource "azurerm_user_assigned_identity" "aibidentity" {
+  location            = var.location
+  name                = "${var.resource_prefix}-${random_string.suffix.result}-identity"
+  resource_group_name = azurerm_resource_group.demo-rg.name
+}
+
+resource "azurerm_role_assignment" "storage-role-assignment" {
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_user_assigned_identity.aibidentity.principal_id
+  scope                = azurerm_resource_group.demo-rg.id
+}
+
+resource "azurerm_role_assignment" "gallery-role-assignment" {
+  role_definition_name = "Compute Gallery Artifacts Publisher"
+  principal_id         = azurerm_user_assigned_identity.aibidentity.principal_id
+  scope                = azurerm_resource_group.demo-rg.id
+}
+
+resource "azurerm_role_assignment" "vnet-role-assignment" {
+  principal_id         = azurerm_user_assigned_identity.aibidentity.principal_id
+  role_definition_name = "Network Contributor"
+  scope                = azurerm_resource_group.demo-rg.id
+}
+
+# Alternatively, you can create custom roles for more granular permission controls.
+
+# resource "azurerm_role_definition" "image_creation_role" {
+#   name        = "Azure Image Builder Service Image Creation Role"
+#   description = "Image Builder access to create resources for the image build, you should delete or split out as appropriate"
+#   scope       = "/subscriptions/${var.subscription_id}"
+#   permissions {
+#     actions = [
+#       "Microsoft.Compute/galleries/read",
+#       "Microsoft.Compute/galleries/images/read",
+#       "Microsoft.Compute/galleries/images/versions/read",
+#       "Microsoft.Compute/galleries/images/versions/write",
+
+#       "Microsoft.Compute/images/write",
+#       "Microsoft.Compute/images/read",
+#       "Microsoft.Compute/images/delete"
+#     ]
+#     not_actions = []
+#   }
+# }
+
+# resource "azurerm_role_assignment" "image_creation_role_assignment" {
+#   role_definition_id = azurerm_role_definition.image_creation_role.role_definition_resource_id
+#   principal_id       = azurerm_user_assigned_identity.aibidentity.principal_id
+#   scope              = azurerm_resource_group.demo-rg.id
+# }
+
+# resource "azurerm_role_definition" "image_creation_vnet_role" {
+#   name = "Azure Image Builder Service Networking Role"
+#   description = "Image Builder vnet access to create resources for the image build"
+#   scope = "/subscriptions/${var.subscription_id}"
+#   permissions {
+#     actions = [
+#       "Microsoft.Network/virtualNetworks/subnets/join/action",
+#       "Microsoft.Network/virtualNetworks/read"
+#     ]
+#     not_actions = []
+#   }
+# }
+
+# resource "azurerm_role_assignment" "image_creation_vnet_role_assignment" {
+#   role_definition_id = azurerm_role_definition.image_creation_vnet_role.role_definition_resource_id
+#   principal_id       = azurerm_user_assigned_identity.aibidentity.principal_id
+#   scope              = azurerm_resource_group.demo-rg.id
+# }
